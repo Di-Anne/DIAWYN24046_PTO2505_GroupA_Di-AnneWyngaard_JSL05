4ff513f461075a013f7a07a1c56390dd473f0467
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-11-20 16:40:49 +0200

    Clearing form input comment

--- scripts/tasks/form.js
+++ scripts/tasks/form.js
@@ -1,3 +1,4 @@
+// Clearing form inputs after submission gives visual evidence form was submitted
 export function resetForm() {
     document.getElementById('addtask-title').value = "";
     document.getElementById('addtask-descrip').value = "";
