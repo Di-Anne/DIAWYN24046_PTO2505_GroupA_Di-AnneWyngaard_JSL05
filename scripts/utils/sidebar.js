1f862b35b1cc608c67676a652e34073e8657060e
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-11-27 01:57:39 +0200

    Hide nav-eyes-toggle when sidebar is visible

--- scripts/utils/sidebar.js
+++ scripts/utils/sidebar.js
@@ -5,9 +5,12 @@
 export function hideSidebar() {
     const hideSidebarBtn = document.getElementById('nav-hide');
     const sidebar = document.getElementById('side-nav-bar');
+    const showSidebarBtn = document.getElementById('nav-eyes');
 
     hideSidebarBtn.addEventListener('click', () => {
         sidebar.style.display = "none";
+        showSidebarBtn.style.display = "block";
+        showSidebarBtn.style
     });
 }
 
